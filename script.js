f9a74c31d243db5dcd7b12846027b27967eb7209
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2023-06-05 18:04:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let h1 = document.createElement("h1");
+let div = document.createElement("div");
+div.id = "sizeInfo";
+div.append(h1);
+let body = document.getElementsByTagName("body")[0];
+body.append(div);
+
+window.onresize = function(){
+    let x = innerWidth;
+    let y = innerHeight;
+    h1.innerText = `Width ${x} Height ${y}`;
+};
